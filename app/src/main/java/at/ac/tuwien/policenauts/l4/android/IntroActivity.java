d8cf9328ffbff36b943f904280d6934ccd671494
Wassily Bartuska <e1427303@student.tuwien.ac.at>
2017-01-07 02:13:05 +0100

    Put intro video in landscape mode and fullscreen.

--- app/src/main/java/at/ac/tuwien/policenauts/l4/android/IntroActivity.java
+++ app/src/main/java/at/ac/tuwien/policenauts/l4/android/IntroActivity.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.policenauts.l4.android;
 
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -32,6 +33,7 @@ public class IntroActivity extends AppCompatActivity implements MediaPlayer.OnCo
         // Initialize act
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         //initialize VideoView:
         VideoView view = (VideoView)findViewById(R.id.videoView);
